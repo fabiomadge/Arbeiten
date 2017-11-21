3b8033ebe3ee61c0a65143b4d11e994bd346efc9
Fabio Madge Pimentel <fabio@madge.me>
2017-11-21 02:31:47 +0100

    additional test

--- Masterseminar/src/Test.hs
+++ Masterseminar/src/Test.hs
@@ -4,8 +4,10 @@ module Test where
 
 import Test.QuickCheck
 import Data.Foldable as F (toList)
+import Data.List as List (sortOn) 
 import FingerTree
 import qualified Seq
+import qualified PQueue
 
 instance Monoid Integer where
     mempty = 0
@@ -34,3 +36,12 @@ breakTree p ns = split p ((toTree:: [Integer] -> FingerTree Integer Integer) ns)
 prop_index :: [Integer] -> [Integer] -> Bool
 prop_index xs ys = xs == Seq.toList xs' && ys == Seq.toList ys'
     where (xs', ys') = Seq.splitAt ((toInteger . Prelude.length) xs) (Seq.fromList xs Seq.>< Seq.fromList ys)
+
+prop_sorted :: [Integer] -> Bool
+prop_sorted xs = List.sortOn (0 -) xs == sorted xs
+    where
+        popAll q = case PQueue.extractMax q of
+            Nothing -> []
+            Just (p,q) -> p : popAll q 
+        pushAll = foldr PQueue.insert PQueue.empty
+        sorted = popAll . pushAll
